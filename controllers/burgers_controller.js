1fc0fc951c833582d2bd171776b6137c17d7d2c8
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-01-03 10:53:22 -0600

    controller updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,35 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res){
+	burger.selectAll(function(data){
+		var burgObj = {
+			burgers: data
+		};
+		res.render('index', burgObj);
+	});
+});
+
+router.post("/burgers", function(req,res){
+	burger.insertOne([
+		'burger_name'
+		], [
+		req.body.burger_name
+		], function(data){
+			res.redirect("/");
+		});
+});
+
+router.put("/burgers/:id", function(req,res){
+	var condition = "id = " + req.params.id;
+
+	console.log("condition: ", condition);
+
+	burger.updateOne({
+		devoured: true
+	}, condition, function(date){
+		res.redirect("/");
+	});
+});
+
+module.exports = router;
